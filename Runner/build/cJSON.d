9d0309a293f6825f42cafcd603c21ff3095cf4f4
Ralcactus <andersonlevi12@outlook.com>
2026-01-31 22:20:46 +1000

    added proper multi platform support (still has 3ds apis in the runner itself)

---
+++ Runner/build/cJSON.d
@@ -0,0 +1,3 @@
+cJSON.o: D:/Projects/GitHub/GameMaker-Anywhere/Runner/source/cJSON.c \
+ D:/Projects/GitHub/GameMaker-Anywhere/Runner/source/cJSON.h
+D:/Projects/GitHub/GameMaker-Anywhere/Runner/source/cJSON.h:
