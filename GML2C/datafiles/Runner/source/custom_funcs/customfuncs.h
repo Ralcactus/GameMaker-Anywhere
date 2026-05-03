ffff75603c3d69844cbced2f292eafe3e130e451
Ralcactus <andersonlevi12@outlook.com>
2026-05-03 10:45:12 +1000

    wtf am i thinking

--- GML2C/datafiles/Runner/source/custom_funcs/customfuncs.h
+++ GML2C/datafiles/Runner/source/custom_funcs/customfuncs.h
@@ -15,15 +15,14 @@
 #include <variant>
 
 #define function void
-#define argument0 int 0
-#define argument1 int 0
-#define argument2 int 0
-#define argument3 int 0
-#define argument4 int 0
-#define argument5 int 0
-#define argument6 int 0
-#define argument7 int 0
-#define argument8 int 0
-#define argument9 int 0
-#define argument [argument0, argument1, argument2, argument3, argument4, argument5, argument6, argument7, argument8, argument9]
-
+#define argument0 int bleh0
+#define argument1 int bleh1
+#define argument2 int bleh2
+#define argument3 int bleh3
+#define argument4 int bleh4
+#define argument5 int bleh5
+#define argument6 int bleh6
+#define argument7 int bleh7
+#define argument8 int bleh8
+#define argument9 int bleh9
+#define argument [bleh0, bleh1, bleh2, bleh3, bleh4, bleh5, bleh6, bleh7, bleh8, bleh9]
--- Test_Project/objects/obj_gamekiller/Step_0.gml
+++ Test_Project/objects/obj_gamekiller/Step_0.gml
@@ -2,3 +2,4 @@ if (gamepad_button_check_pressed(0, gp_start)){
 	show_message("KILLING THE GAME HAHAAH!!");
 	game_end();
 }
+show_debug_message(id);
