68657d79b2d3082c2a8cf5826ba6bc519d59ffea
Ralcactus <andersonlevi12@outlook.com>
2026-08-30 15:49:05 +1000

    cleaned up sprite compiler

--- source/compiler/Asset/SpriteCompiler.cpp
+++ source/compiler/Asset/SpriteCompiler.cpp
@@ -14,6 +14,35 @@
 
 char SpriteName[256] = "";
 
+//Write to a sprite list (EG: SCF, T3S)
+void WriteSpriteList(std::string frame_name, int i){
+    //T3S sprite list
+    if (strcmp(ExportMode, "3DSX") == 0 || strcmp(ExportMode, "CIA") == 0){
+        char SpriteFile[256];
+        snprintf(SpriteFile, sizeof(SpriteFile), "%s.png\n", frame_name.c_str());
+        File_WriteEnd("C:/GamemakerAnywhere/Runtime/gfx/sprites.t3s", SpriteFile);
+    }
+
+    //SCF sprite list
+    if (strcmp(ExportMode, "GAMECUBE") == 0 || strcmp(ExportMode, "WII") == 0){
+        char TextureLine[512];
+        snprintf(TextureLine, sizeof(TextureLine), "<filepath=%s.png id=\"%sFSDSDFFDGIOJHDFIOHEFAMILYGUY2DDD%d%d\" colfmt=6 />\n", frame_name.c_str(), SpriteName, rand() % 100, i); //for the sprite name, were just gonna use the custom defines so i just made it strange
+        File_WriteEnd("C:/GamemakerAnywhere/Runtime/gfx/textures.scf", TextureLine);
+    }
+}
+
+void CopyImageFile(std::string frame_name){
+    char ProjectDir[256];
+    char ImagePath[256];
+    char CopyCommand[512];
+
+    snprintf(ProjectDir, sizeof(ProjectDir), "%s", ProjectYYP);
+    *strrchr(ProjectDir, '\\') = '\0';
+    snprintf(ImagePath, sizeof(ImagePath), "%s/sprites/%s/%s.png", ProjectDir, SpriteName, frame_name.c_str());
+    snprintf(CopyCommand, sizeof(CopyCommand), "powershell -Command \"Copy-Item -Path '%s' -Destination 'C:/GamemakerAnywhere/Runtime/gfx'\"", ImagePath);
+    system(CopyCommand);
+}
+
 void scr_compilesprites(Json::Value yyfile){
     printf("Compiling Sprite...\n");
 
@@ -26,34 +55,15 @@ void scr_compilesprites(Json::Value yyfile){
     for (int i = 0; i < ARRAYSIZE(yyfile["frames"]); i++) {
         std::string frame_name = yyfile["frames"][i]["name"].asString();
 		currentsprite_count++;
-
-        char ProjectDir[256];
-        char ImagePath[256];
-        char CopyCommand[512];
         
-        snprintf(ProjectDir, sizeof(ProjectDir), "%s", ProjectYYP);
-        *strrchr(ProjectDir, '\\') = '\0';
-        snprintf(ImagePath, sizeof(ImagePath), "%s/sprites/%s/%s.png", ProjectDir, SpriteName, frame_name.c_str());
-        snprintf(CopyCommand, sizeof(CopyCommand), "powershell -Command \"Copy-Item -Path '%s' -Destination 'C:/GamemakerAnywhere/Runtime/gfx'\"", ImagePath);
-        system(CopyCommand);
+        CopyImageFile(frame_name);
         
 		printf("Saving sprite info...\n");
 		//scr_savesprite_info(frame_name);
 		printf("Saved!\n");
 		
-		//T3S sprite list
-        if (strcmp(ExportMode, "3DSX") == 0 || strcmp(ExportMode, "CIA") == 0){
-            char SpriteFile[256];
-            snprintf(SpriteFile, sizeof(SpriteFile), "%s.png\n", frame_name.c_str());
-            File_WriteEnd("C:/GamemakerAnywhere/Runtime/gfx/sprites.t3s", SpriteFile);
-        }
-
-		//SCF sprite list
-        if (strcmp(ExportMode, "GAMECUBE") == 0 || strcmp(ExportMode, "WII") == 0){
-            char TextureLine[512];
-            snprintf(TextureLine, sizeof(TextureLine), "<filepath=%s.png id=\"%sFSDSDFFDGIOJHDFIOHEFAMILYGUY2DDD%d%d\" colfmt=6 />\n", frame_name.c_str(), SpriteName, rand() % 100, i); //for the sprite name, were just gonna use the custom defines so i just made it strange
-            File_WriteEnd("C:/GamemakerAnywhere/Runtime/gfx/textures.scf", TextureLine);
-        }
+        //Write to a sprite list (EG: SCF, T3S)
+        WriteSpriteList(frame_name, i);
     }
 	currentsprite_count--;
 	
